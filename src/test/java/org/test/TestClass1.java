578333666b9bb9cc0c4e788ce46d6fb7d28b6236
AbdulGitt <greenstechnology12345@gmail.com>
2025-06-28 13:46:19 +0530

    arun new code

--- src/test/java/org/test/TestClass1.java
+++ src/test/java/org/test/TestClass1.java
@@ -5,6 +5,7 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Sample Project Structure");
+		System.out.println("Arun new code in 8th line");
 		
 	}
 
